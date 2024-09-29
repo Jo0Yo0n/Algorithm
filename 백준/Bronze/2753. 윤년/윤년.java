cdde69f09b4695f4e01fe3f8c94b350416e77586
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2024-09-30 01:00:05 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int year = Integer.parseInt(br.readLine());
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            bw.write(1 + "\n");
+        }
+        else {
+            bw.write(0 + "\n");
+        }
+
+        br.close();
+        
+        bw.flush();
+        bw.close();
+    }
+}
