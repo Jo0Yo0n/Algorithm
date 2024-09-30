c957a89814b8b29387a046a6e371a5a6db20b4d9
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2024-10-01 02:04:17 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 820 ms, Memory: 258652 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -1,22 +1,12 @@
 import java.util.*;
 import java.io.*;
 
-// 1. 테스트 케이스의 개수를 입력받는다.
-// 2. 테스트 케이스의 개수만큼 for문으로 반복한다.
-//   2-1. 정수의 개수(N)를 입력받는다.
-//   2-2. N개의 정수를 공백으로 구분해서 입력받아 배열(arr)에 저장한다.
-//   2-3. 최댓값, 최솟값 변수를 선언하고 각각 -1000001, 1000001로 초기화 한다.
-//   2-4. N만큼 for문으로 반복한다.
-//     2-4-1. arr[i]에 저장된 정수가 최솟값보다 작으면 최솟값을 arr[i]로 바꾼다.
-//     2-4-2. arr[i]에 저장된 정수가 최댓값보다 크다면 최댓값을 arr[i]로 바꾼다.
-//   2-5. 최솟값과 최댓값을 공백으로 구분해 출력한다.
-
 public class Main {
     
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int testCase = Integer.parseInt(br.readLine());
 
@@ -35,6 +25,7 @@ public class Main {
             int max = -1000001;
 
             for(int j = 0; j < N; j++) {
+                
                 if (arr[j] < min) {
                     min = arr[j];
                 }
@@ -44,11 +35,11 @@ public class Main {
                 }
             }
 
-            bw.write(min + " " + max + "\n");
-            bw.flush();
+            sb.append(min).append(" ").append(max).append("\n");
         }
 
+        System.out.print(sb);
+
         br.close();
-        bw.close();
     }
 }
