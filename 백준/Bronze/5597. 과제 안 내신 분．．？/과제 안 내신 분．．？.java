12df06648f2668867591863be6035bd42bdeaee7
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2024-10-01 00:55:50 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 112 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[31];
+
+        for(int i = 0; i < 28; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for(int i = 1; i <= 30; i++) {
+            if (arr[i] == 0) {
+                bw.write(i + "\n");
+            }
+        }
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
