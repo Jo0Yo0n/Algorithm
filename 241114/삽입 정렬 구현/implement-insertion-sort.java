17f1b1ccbee77108ff347788666b59db2466c159
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2024-11-14 20:11:19 +0900

    Title: 삽입 정렬 구현, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241114/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i < n; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
