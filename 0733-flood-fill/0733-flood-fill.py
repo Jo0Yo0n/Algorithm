1e39907d26708ac237bffdf5a7782e0d57e58fcc
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2025-09-16 23:13:38 +0900

    Time: 0 ms (100%), Space: 18 MB (49.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,29 +1,26 @@
-import queue
-
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         start_color = image[sr][sc]
         if start_color == color:
             return image
 
-        dx = [-1, 0, 1, 0]
-        dy = [0, 1, 0, -1]
+        dr = [-1, 0, 1, 0]
+        dc = [0, 1, 0, -1]
         row_size = len(image)
         col_size = len(image[0])
-        q = queue.Queue()
-        q.put((sr, sc))
+        q = deque([(sr, sc)])
         
-        while q.qsize() > 0:
-            cx, cy = q.get()
-            image[cx][cy] = color
+        while q:
+            r, c = q.popleft()
+            image[r][c] = color
 
             for i in range(4):
-                ax = cx + dx[i]
-                ay = cy + dy[i]
-                if ax < 0 or ax >= row_size or ay < 0 or ay >= col_size:
+                ar = r + dr[i]
+                ac = c + dc[i]
+                if ar < 0 or ar >= row_size or ac < 0 or ac >= col_size:
                     continue
-                if image[ax][ay] != start_color or image[ax][ay] == color:
+                if image[ar][ac] != start_color or image[ar][ac] == color:
                     continue
-                q.put((ax, ay))
+                q.append((ar, ac))
 
         return image
