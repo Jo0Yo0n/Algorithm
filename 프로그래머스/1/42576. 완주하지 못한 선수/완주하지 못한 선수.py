a9dfc26e30c166401398f96f04f75b9bcc5df450
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2025-10-09 17:59:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.97 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,15 @@
+def solution(participant, completion):
+    dic = {}
+    for p in participant:
+        if p in dic:
+            dic[p] += 1
+        else:
+            dic[p] = 1
+    
+    for c in completion:
+        if dic[c] == 1:
+            dic.pop(c)
+        else:
+            dic[c] -= 1
+    
+    return list(dic.keys())[0]
