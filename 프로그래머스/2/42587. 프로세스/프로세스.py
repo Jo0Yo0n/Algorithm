2776cf738e47408b243f3dc5354a923f908bc929
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2025-11-05 17:34:54 +0900

    [level 2] Title: 프로세스, Time: 0.81 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,26 +1,13 @@
 def solution(priorities, location):
-    temp = [False for _ in range(len(priorities))]
-    temp[location] = True
-    
-    p_queue = list(zip(priorities, temp))
-    
-    d = {}
-    for p in priorities:
-        d[p] = d.get(p, 0) + 1
-    
+    q = [(i, p) for (i, p) in enumerate(priorities)]
     ans = 0
+    
     while True:
-        flag = True
-        
-        for i in range(p_queue[0][0] + 1, 10):
-            if d.get(i, 0) > 0:
-                p_queue.append(p_queue.pop(0))
-                flag = False
-                break
+        temp = q.pop(0)
+        if any(temp[1] < other for (_, other) in q):
+            q.append(temp)
             
-        if flag:
-            d[p_queue[0][0]] -= 1
+        else:
             ans += 1
-            t = p_queue.pop(0)
-            if t[1]:
+            if temp[0] == location:
                 return ans
