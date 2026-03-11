15fd97abf0af22d185ceae05792ec2248c818d72
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2026-03-11 14:00:39 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 9.86 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,4 @@
 def solution(nums):
-    nums.sort()
-    lst = []
-    
-    for i in range(len(nums)):
-        if len(lst) == (len(nums) // 2):
-            return len(lst)
-        
-        if i > 0 and (nums[i] == nums[i - 1]):
-            continue
-        
-        lst.append(nums[i])
-    
-    return len(lst)
+    max_value = len(nums) // 2
+    s = set(nums)
+    return len(s) if max_value > len(s) else max_value
