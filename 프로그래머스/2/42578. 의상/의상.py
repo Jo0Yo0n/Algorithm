94becd2640f22d16e258296b8eb702b51e2e154c
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2026-03-23 17:23:05 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,11 @@
 def solution(clothes):
-    c_dic = {}
+    clothes_dict = {}
     
     for c in clothes:
-        c_dic[c[1]] = c_dic.get(c[1], 0) + 1
-    
+        clothes_dict[c[1]] = clothes_dict.get(c[1], 0) + 1
+        
     ans = 1
-    for k in c_dic:
-        ans *= c_dic[k] + 1
-    
+    for item in clothes_dict.values():
+        ans *= item + 1
+        
     return ans - 1
