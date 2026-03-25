4e4e6fe1cfdeb1966c0ed7ea8044044fef5f870f
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2026-03-25 13:07:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.74 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 27.2 MB, 시간: 141.28 ms
+메모리: 27.1 MB, 시간: 59.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 15:46:44
+2026년 03월 25일 13:06:57
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,11 +1,12 @@
+from collections import deque
+
 def solution(arr):
-    ans = []
-    ans.append(arr[0])
+    answer = []
+    answer.append(arr[0])
     
-    for i in range(1, len(arr)):
-        if arr[i - 1] == arr[i]:
+    for a in arr:
+        if answer[-1] == a:
             continue
+        answer.append(a)
         
-        ans.append(arr[i])
-        
-    return ans
+    return answer
