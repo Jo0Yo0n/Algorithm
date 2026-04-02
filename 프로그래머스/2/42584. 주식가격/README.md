df738f1173b3c737ed45f52aa2368dde1e9e9980
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2026-04-02 12:50:36 +0900

    [level 2] Title: 주식가격, Time: 35.12 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 18.9 MB, 시간: 36.71 ms
+메모리: 19 MB, 시간: 35.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 21:07:54
+2026년 04월 02일 12:50:32
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,12 @@
 def solution(prices):
     answer = [0] * len(prices)
-    
     stack = []
-    for i, p in enumerate(prices):
-        while stack and stack[-1][1] > p:
-            answer[stack[-1][0]] = i - stack[-1][0]
-            stack.pop()
+    
+    for idx, price in enumerate(prices):
+        while stack and (stack[-1][1] > price or idx == len(prices) - 1):
+            i, p = stack.pop()
+            answer[i] = idx - i
         
-        stack.append((i, p))
-    
-    duration = len(prices) - 1
-    
-    while stack:
-        answer[stack[-1][0]] = duration - stack[-1][0]
-        stack.pop()
-    
+        stack.append((idx, price))
+                         
     return answer
