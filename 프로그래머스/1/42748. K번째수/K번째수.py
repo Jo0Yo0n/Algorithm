2a7ee165bf0ce9590e241d9cc2114f90e990cef2
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2026-04-02 14:05:34 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,7 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        start, end, seq = command
-        silced_list = array[start - 1:end]
-        silced_list.sort()
-        answer.append(silced_list[seq - 1])
+    for start, end, seq in commands:
+        silced_and_sorted_list = sorted(array[start - 1:end])
+        answer.append(silced_and_sorted_list[seq - 1])
         
     return answer
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 9.08 MB, 시간: 0.01 ms
+메모리: 9.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 02일 13:58:30
+2026년 04월 02일 14:05:31
 
 ### 문제 설명
 
