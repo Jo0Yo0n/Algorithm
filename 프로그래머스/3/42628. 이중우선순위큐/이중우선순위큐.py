db168517dc0845eb9d0991b6e67453941ba7472b
JooYoon <68751201+Jo0Yo0n@users.noreply.github.com>
2026-04-21 17:01:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 137.11 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,45 +1,39 @@
 import heapq
 
 def solution(operations):
-    min_heap = []
+    heap_size = 0
     max_heap = []
-    is_deleted = {}
-    op_id = 0
+    min_heap = []
+    visited = [False] * len(operations)
     
-    for operation in operations:
-        o, d = operation.split()
+    for idx, op in enumerate(operations):
+        cmd, value = op.split()
+        value = int(value)
         
-        if o == "I":
-            d = int(d)
-            heapq.heappush(min_heap, (d, op_id))
-            heapq.heappush(max_heap, (-d, op_id))
-            is_deleted[op_id] = False
-            op_id += 1
+        if cmd == 'I':
+            heapq.heappush(max_heap, (-value, idx))
+            heapq.heappush(min_heap, (value, idx))
             
         else:
-            if d == "1":
-                while max_heap and is_deleted[max_heap[0][1]]:
+            if value == 1:
+                while max_heap and visited[max_heap[0][1]]:
                     heapq.heappop(max_heap)
-                
                 if max_heap:
-                    is_deleted[heapq.heappop(max_heap)[1]] = True
-                    
+                    _, idx = heapq.heappop(max_heap)
+                    visited[idx] = True
             else:
-                while min_heap and is_deleted[min_heap[0][1]]:
+                while min_heap and visited[min_heap[0][1]]:
                     heapq.heappop(min_heap)
-                
                 if min_heap:
-                    is_deleted[heapq.heappop(min_heap)[1]] = True
-
-    while max_heap and is_deleted[max_heap[0][1]]:
-        heapq.heappop(max_heap)
-    
-    while min_heap and is_deleted[min_heap[0][1]]:
-        heapq.heappop(min_heap)
-    
-    if min_heap and max_heap:
-        return [-max_heap[0][0], min_heap[0][0]]
-    else:
-        return [0, 0]
+                    _, idx = heapq.heappop(min_heap)
+                    visited[idx] = True
         
-            
+    while max_heap and visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+    while min_heap and visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+        
+    if not max_heap and not min_heap:
+        return [0, 0]
+    
+    return [-max_heap[0][0], min_heap[0][0]]
